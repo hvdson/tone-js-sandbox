11b5d6519ce02d9c47bb6ed1b35259ffa76119a1
hvdson <hvdsonmusic@gmail.com>
2018-03-30 20:25:14 -0700

    small change to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tone from 'tone'
 
 class App extends Component {
+
+  _newSynth() {
+    //create a synth and connect it to the master output (your speakers)
+    var synth = new Tone.Synth().toMaster()
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n')
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +21,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          IT's LIT
+          {this._newSynth}
         </p>
       </div>
     );
