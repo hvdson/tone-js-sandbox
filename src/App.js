006f04a4137f33b99c31f017f8516f140ddb4092
hvdson <hvdsonmusic@gmail.com>
2018-03-31 15:18:07 -0700

    have a button generating a pulse wave which will toggle start or stop

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import './App.css';
 import Tone from 'tone'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.omniOsc = new Tone.OmniOscillator("C#4", "pwm").toMaster();
+    this.state = {
+      oscillatorOn: false
+    };
+  }
 
-  _newSynth() {
+  _handleClick = () => {
+    if (this.state.oscillatorOn) {
+      this.omniOsc.stop();
+      this.setState({ oscillatorOn: false });
+    } else {
+      this._newSynth();
+    }
+  }
+
+  _newSynth = () => {
     //create a synth and connect it to the master output (your speakers)
-    var synth = new Tone.Synth().toMaster()
-
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n')
+    this.omniOsc.start();
+    this.setState({oscillatorOn: true});
   }
 
   render() {
@@ -21,8 +35,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          IT's LIT
-          {this._newSynth}
+          <button onClick={this._handleClick}>IT's LIT</button>
         </p>
       </div>
     );
